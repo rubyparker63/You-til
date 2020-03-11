79966f9e33bb125b5fb302f275a3d347e56e930f
rubyparker63 <ruby.parker.63@my.csun.edu>
2020-03-11 13:23:00 -0700

    Initial commit

---
+++ app/src/main/java/comp380/csun/you_til/ConversionActivity.java
@@ -0,0 +1,16 @@
+package comp380.csun.you_til;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp380.csun.you_til.R;
+
+public class ConversionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversion);
+    }
+}
