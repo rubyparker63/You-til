17e612342eca004c1c347a6b6f8abfa191e3167d
rubyparker63 <ruby.parker.63@my.csun.edu>
2020-03-25 17:49:37 -0700

    Commit 3 Working on Dice display, and updated colors for use by reference.

---
+++ app/src/main/java/comp380/csun/you_til/DiceRollActivity4.java
@@ -0,0 +1,14 @@
+package comp380.csun.you_til;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiceRollActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice_roll4);
+    }
+}
