8704cc84aad0ea1303f0d945480b1ada6b2fc46b
Ani Khachatryan <anikhachatryan@Anis-MacBook-Pro.local>
2020-04-06 21:40:03 -0700

    Coin Flip commit

--- app/src/main/java/comp380/csun/you_til/CoinFLipActivity.java
+++ app/src/main/java/comp380/csun/you_til/CoinFLipActivity.java
@@ -3,6 +3,10 @@ package comp380.csun.you_til;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
 
 import comp380.csun.you_til.R;
 
@@ -12,5 +16,26 @@ public class CoinFLipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_f_lip);
+
+        final TextView coinTxt = (TextView)findViewById(R.id.coinFlipButton);
+
+        ImageView Coin = (ImageView)findViewById(R.id.imageView5);
+        Coin.setImageResource(R.drawable.coin);
+
+        Button flipButton = (Button)findViewById(R.id.coinFlipButton);
+        flipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                coinTxt.setText("");
+                int rand = (int)(Math.random()*2);
+
+                if(rand == 0) {
+                    coinTxt.setText("Heads");
+                }
+                else {
+                    coinTxt.setText("Tails");
+                }
+            }
+        });
     }
 }
