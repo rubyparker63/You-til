495007941e08a26aa375284897b9b14dad862647
rubyparker63 <ruby.parker.63@my.csun.edu>
2020-04-06 19:55:11 -0700

    Commit 4 Updated main menu so it is actually usable and not just decoration. Still working on Dice display, having a problem with the images not showing up.

--- app/src/main/java/comp380/csun/you_til/MainActivity.java
+++ app/src/main/java/comp380/csun/you_til/MainActivity.java
@@ -2,9 +2,10 @@ package comp380.csun.you_til;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import comp380.csun.you_til.R;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calcActivityBtn = (Button) findViewById(R.id.calcMainButton);
+        calcActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent goToCalc = new Intent(getApplicationContext(), CalculatorActivity.class);
+                startActivity(goToCalc);
+            }
+        });
+        Button coinFlipActivityBtn = (Button) findViewById(R.id.coinMainButton);
+        coinFlipActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent goToCoin = new Intent(getApplicationContext(), CoinFLipActivity.class);
+                startActivity(goToCoin);
+            }
+        });
+
+        Button conversionActivityBtn = (Button) findViewById(R.id.conversionMainButton);
+        conversionActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToConversion = new Intent(getApplicationContext(), ConversionActivity.class);
+                startActivity(goToConversion);
+            }
+        });
+
+        Button diceRollActivityBtn = (Button) findViewById(R.id.diceMainButton);
+        diceRollActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToDiceSelect = new Intent(getApplicationContext(), DiceRollSelectActivity.class);
+                startActivity(goToDiceSelect);
+            }
+        });
+
+        Button tipCalcActivityBtn = (Button) findViewById(R.id.tipCalcMainButton);
+        tipCalcActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToTipCalc = new Intent(getApplicationContext(), TipCalculatorActivity.class);
+                startActivity(goToTipCalc);
+            }
+        });
+
     }
 }
