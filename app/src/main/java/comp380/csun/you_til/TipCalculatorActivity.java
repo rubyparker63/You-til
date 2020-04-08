aac7996163674a19d5a64b3a0c5a9e1420241b57
rubyparker63 <ruby.parker.63@my.csun.edu>
2020-04-07 21:43:08 -0700

    Commit 12: Basic Tempurature outline setup.

--- app/src/main/java/comp380/csun/you_til/TipCalculatorActivity.java
+++ app/src/main/java/comp380/csun/you_til/TipCalculatorActivity.java
@@ -3,6 +3,10 @@ package comp380.csun.you_til;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import comp380.csun.you_til.R;
 
@@ -12,5 +16,20 @@ public class TipCalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calculator);
+
+
+        //Button btn = (Button) findViewById(R.id.btn);
+        //btn.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View v) {
+           //     TextView textView = (TextView) findViewById(R.id.tv3);
+            //    EditText et1 = (EditText) findViewById(R.id.et1);
+              //  EditText et2 = (EditText) findViewById(R.id.et2);
+              //  double bill = Double.valueOf(et1.getText().toString());
+               // double tipPercent = Double.valueOf(et2.getText().toString());
+               // textView.setText("Tip amount is: $" + (bill * tipPercent / 100));
+           // }
+       // });
+
     }
 }
